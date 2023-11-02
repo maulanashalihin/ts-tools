922be3a7ddc81d3f7ee2c7b475fbf37ebf922628
MasRama <ryushineo@gmail.com>
2023-11-02 18:29:24 +0700

    switch toggle

--- resources/js/Pages/omoo-stats.svelte
+++ resources/js/Pages/omoo-stats.svelte
@@ -658,21 +658,24 @@
 
     {:else if model == "konten"}
     
-<label class="relative inline-flex items-center cursor-pointer">
-  <input type="checkbox" value="" class="sr-only peer" on:click={() => {
-    stratplan = !stratplan
-    //select sesuai act
-    if(act == "7day") loadDataTrending(dayjs().subtract(7, "day"), dayjs())
-    else if(act == "30day") loadDataTrending(dayjs().subtract(30, "day"), dayjs())
-    else if(act == "custom") {
-      let start_date = dayjs(document.getElementById("datepicker").value.split(" - ")[0])
-      let end_date = dayjs(document.getElementById("datepicker").value.split(" - ")[1])
-      loadDataTrending(start_date, end_date)
-    }
-  }} >
-  <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300  rounded-full peer  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-blue-600"></div>
-  <span class="ml-3 text-sm font-medium text-gray-500 ">Filter Konten Sesuai Strat Plan OMOO</span>
-</label>
+    
+    <div class="inline-flex items-center space-x-3">
+      <input type="checkbox" id="tk-form-switches-with-labels-small"  class="form-switch transition-all duration-150 ease-out rounded-full h-6 w-10 text-sky-600" on:click={() => {
+        stratplan = !stratplan
+        //select sesuai act
+        if(act == "7day") loadDataTrending(dayjs().subtract(7, "day"), dayjs())
+        else if(act == "30day") loadDataTrending(dayjs().subtract(30, "day"), dayjs())
+        else if(act == "custom") {
+          let start_date = dayjs(document.getElementById("datepicker").value.split(" - ")[0])
+          let end_date = dayjs(document.getElementById("datepicker").value.split(" - ")[1])
+          loadDataTrending(start_date, end_date)
+        }
+      }} />
+      <label for="tk-form-switches-with-labels-small" class="text-sm text-gray-500 font-medium">
+        Filter Konten Sesuai Strat Plan OMOO
+      </label>
+    </div>
+
 
     <div class="grid lg:grid-cols-3 grid-cols-1 gap-4 mt-5 z-0">
       {#each trending as item, index}
