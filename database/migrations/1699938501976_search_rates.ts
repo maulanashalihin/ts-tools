4a55d09a5295fcf5e2ea4ce5dec3524ce85cd4b8
MasRama <ryushineo@gmail.com>
2023-11-14 12:08:55 +0700

    add : search rates table

---
+++ database/migrations/1699938501976_search_rates.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'search_rates'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string("date").index()
+      table.string("city").index();
+      table.integer("troop_id").index();
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
