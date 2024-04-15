27881c6eb575fddaa658a4c949ad627aa6ecf20e
maulana <maulana@drip.id>
2024-04-15 08:18:26 +0800

    step data

--- build/commands/MigrateDb.js
+++ build/commands/MigrateDb.js
@@ -7,11 +7,22 @@ const standalone_1 = require("@adonisjs/core/build/standalone");
 const Database_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Database"));
 class MigrateDb extends standalone_1.BaseCommand {
     async run() {
-        const headers = ["popup_contents"];
+        const headers = ["omoo_histories", "open_rates", "share_rates"];
         for await (const table of headers) {
-            const data = await Database_1.default.connection("mysql").from(table);
-            for await (const row of data) {
-                await Database_1.default.table(table).insert(row);
+            let latest_id = 0;
+            let total = await Database_1.default.from(table).count("* as total").first();
+            if (total && total.total > 0) {
+                let data;
+                if (latest_id = 0) {
+                    data = await Database_1.default.connection("mysql").from(table).limit(1000);
+                }
+                else {
+                    data = await Database_1.default.connection("mysql").from(table).where("id", ">", latest_id).limit(1000);
+                }
+                for await (const row of data) {
+                    await Database_1.default.table(table).insert(row);
+                    latest_id = row.id;
+                }
             }
             console.log("done migrate " + table + " table");
         }
--- commands/MigrateDb.ts
+++ commands/MigrateDb.ts
@@ -31,13 +31,30 @@ export default class MigrateDb extends BaseCommand {
 
  
 
-    const headers = ["popup_contents"];
+    const headers = ["omoo_histories","open_rates","share_rates"];
 
     for await (const table of headers) {
-      const data = await Database.connection("mysql").from(table);
-      for await (const row of data) {
-        await Database.table(table).insert(row);
+      let latest_id = 0;
+      let total  = await Database.from(table).count("* as total").first();
+
+      if(total && total.total > 0)
+      {
+        let data;
+
+        if(latest_id = 0)
+        {
+          data = await Database.connection("mysql").from(table).limit(1000);
+        }
+        else {
+          data = await Database.connection("mysql").from(table).where("id",">",latest_id).limit(1000);
+        }
+        
+        for await (const row of data) {
+          await Database.table(table).insert(row);
+          latest_id = row.id;
+        }
       }
+      
      
       console.log("done migrate "+table+" table");
     }
