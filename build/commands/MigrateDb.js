7633fa698ada1c39e6f7f45313d8c3b103f80e2b
maulana <maulana@drip.id>
2024-04-15 08:06:04 +0800

    popup content

--- build/commands/MigrateDb.js
+++ build/commands/MigrateDb.js
@@ -7,7 +7,7 @@ const standalone_1 = require("@adonisjs/core/build/standalone");
 const Database_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Lucid/Database"));
 class MigrateDb extends standalone_1.BaseCommand {
     async run() {
-        const headers = ["users", "campaigns", "troops", "tweets", "medias", "tweet_to_buzzes", "campaign_attendances", "api_keys", "messages", "riayahs", "channels", "contents", "channel_admins", "api_tokens", "bank_tsaqofahs", "strat_plans", "request_unblocks", "open_rates", "share_rates", "search_rates", "popup_contents"];
+        const headers = ["popup_contents"];
         for await (const table of headers) {
             const data = await Database_1.default.connection("mysql").from(table);
             for await (const row of data) {
--- commands/MigrateDb.ts
+++ commands/MigrateDb.ts
@@ -31,7 +31,7 @@ export default class MigrateDb extends BaseCommand {
 
  
 
-    const headers = ["users","campaigns","troops","tweets","medias","tweet_to_buzzes","campaign_attendances","api_keys","messages","riayahs","channels","contents","channel_admins","api_tokens","bank_tsaqofahs","strat_plans","request_unblocks","open_rates","share_rates","search_rates","popup_contents"];
+    const headers = ["popup_contents"];
 
     for await (const table of headers) {
       const data = await Database.connection("mysql").from(table);
